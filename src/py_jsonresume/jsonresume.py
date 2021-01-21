835a727657036cd3b7a5f2c035477ff948b57485
Burton DeWilde <burtdewilde@gmail.com>
2021-01-21 10:01:15 -0600

    Add schema validation to main class

--- src/py_jsonresume/jsonresume.py
+++ src/py_jsonresume/jsonresume.py
@@ -1,11 +1,17 @@
 import io
 import json
-from typing import Any, Dict
+import logging
+import urllib.requests
+from typing import Any, Dict, Optional, Union
 
 import jinja2
+import jsonschema
 import weasyprint
 
 from py_jsonresume import filters
+
+
+LOGGER = logging.getLogger(__name__)
 
 
 class JsonResume:
@@ -26,17 +32,43 @@ class JsonResume:
         env.filters["handle_md_inline_code"] = filters.handle_md_inline_code
         return env
 
-    def _load_json_data(self, fpath: str) -> Dict[str, Any]:
-        with io.open(fpath, mode="rt") as f:
-            data = json.load(f)
-        return data
+    def validate(self, schema: Optional[Union[str, Dict[str, Any]]] = None):
+        if schema is None:
+            try:
+                schema = self.data["$schema"]
+            except KeyError:
+                LOGGER.error(
+                    "a schema must be specified for validation, either as method arg "
+                    "or '$schema' key in json resume data"
+                )
+                raise
+        if isinstance(schema, str):
+            schema = self._request_json_data(url=schema)
+        try:
+            jsonschema.validate(self.data, schema)
+            LOGGER.info("resume JSON data is valid!")
+        except (jsonschema.ValidationError, jsonschema.SchemaError):
+            LOGGER.exception()
+            raise
 
     def to_html(self, fpath: str):
         html_str = self.template.render(self.data)
         with io.open(fpath, mode="wt") as f:
             f.write(html_str)
+        LOGGER.info("resume saved as HTML to %s", fpath)
 
     def to_pdf(self, fpath: str):
         html_str = self.template.render(self.data)
         html = weasyprint.HTML(string=html_str)
         html.write_pdf(fpath)
+        LOGGER.info("resume saved as PDF to %s", fpath)
+
+    def _load_json_data(self, fpath: str) -> Dict[str, Any]:
+        with io.open(fpath, mode="rt") as f:
+            data = json.load(f)
+        return data
+
+    def _request_json_data(self, url: str):
+        with urllib.request.urlopen(url) as resp:
+            html = resp.read().decode(resp.headers.get_param("charset") or "utf-8")
+        return json.loads(html)
